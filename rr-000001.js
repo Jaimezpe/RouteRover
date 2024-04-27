cfa59fbd43cd9341cd803274c1ae5c901050334f
Jaime <85058301+Jaimezpe@users.noreply.github.com>
2024-04-27 20:12:01 +0200

    Delete rr-000001.js

--- rr-000001.js
+++
@@ -1,9 +0,0 @@
-
-var newPage = "https://www.waitlistr.com/lists/c24c3847/routerover?ref=btnlnk";
-var waittime = 1;
-
-function redirection() {
-    window.location.href = newPage;
-}
-
-setTimeout(redirection, waittime);
