3c6aa5bcf7f64b6ca6a16e1e0ead6ab0fedfb6bb
Jaime <85058301+Jaimezpe@users.noreply.github.com>
2024-04-27 18:17:25 +0200

    /asdfj

---
+++ rr-000001.js
@@ -0,0 +1,9 @@
+
+var newPage = "https://www.waitlistr.com/lists/c24c3847/routerover?ref=btnlnk";
+var waittime = 1;
+
+function redirection() {
+    window.location.href = newPage;
+}
+
+setTimeout(redirection, waittime);
